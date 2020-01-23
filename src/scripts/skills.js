c5dae9787eb54e973a5e2b25d266b35376eb5807
Sannikov Denis <kaktus.Zzzz@mail.ru>
2020-01-23 14:48:10 +0700

    Исправлены ошибки
    Добавлен параллах эффект в первые две секции. Перевёрстана секция отзывы и форма. Выровнены отступы.

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,13 +1,61 @@
-
 const circle = document.querySelector('.circle__round');
 const radius = circle.r.baseVal.value;
 const circumference = 2 * Math.PI * radius;
 
 circle.style.strokeDasharray = `${circumference} ${circumference}`;
 circle.style.strokeDashoffset = circumference;
+
 function setProgress(percent) {
     const offset = circumference - percent / 100 * circumference;
-    circle.style.strokeDashoffset = offset; 
+    circle.style.strokeDashoffset = offset;
 }
 
 setProgress(35);
+
+const parallax = document.querySelector('.parallax');
+const layers = parallax.children;
+
+function moveLayersDependsOnScroll(wScroll) {
+
+    Array.from(layers).forEach(layer => {
+        const divider = layer.dataset.speed;
+        const strafe = wScroll * divider / 10;
+
+        layer.style.transform = `translateY(-${strafe}%)`;
+
+
+    })
+
+}
+
+window.addEventListener('scroll', e => {
+    const wScroll = window.pageYOffset;
+
+    moveLayersDependsOnScroll(wScroll);
+});
+
+
+// const parallaxBudda = document.querySelector('.parallax-budda');
+// const layersBudda = parallaxBudda.children;
+
+// function moveLayersBuddaDependsOnScroll(wScroll) {
+
+//     Array.from(layersBudda).forEach(layerBudda => {
+//         const divider = layerBudda.dataset.speedbudda;
+//         const wScrollNull = wScroll === 1;
+//         const strafe = wScrollNull * divider;
+
+//         layerBudda.style.transform = `translateY(-${strafe}%)`;
+
+
+//     })
+// }
+
+// window.addEventListener('scroll', e => {
+//     const wScroll = window.pageYOffset;
+//     console.log(wScroll);
+//     if (wScroll >= 3004) {
+//         moveLayersBuddaDependsOnScroll(wScroll); 
+//     }
+// });
+// console.log('fff0');
